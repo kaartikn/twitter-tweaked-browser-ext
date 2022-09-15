d1e72767b83345366b09b5f6347bcb5cb09b961b
unknown <kaartik@counselab.com>
2022-09-15 21:59:31 +0100

    Extracted border color from twitter for browser ext

--- src/content.js
+++ src/content.js
@@ -1,50 +1,77 @@
 var backgroundColor = document.body.style.backgroundColor;
+var borderColor;
 
-window.addEventListener("load", function load(event){
-    chrome.storage.local.get({isTwitterTweakedVisible: true}, function(result){
-        toggleTwitterSidePanel(result.isTwitterTweakedVisible);
-        toggleTwitterTweaked(result.isTwitterTweakedVisible);
-        chrome.storage.local.set({isTwitterTweakedVisible: result.isTwitterTweakedVisible});
-    })
-    this.window.removeEventListener("load", load, false);
-})
+handleEventsPostLoading();
+
+setupOnMessageListener();
+
+var iframe = setupSidePanelCSS();
 
 
+// document.addEventListener("DOMContentLoaded", function(e) {
+//     // May have to move away from identifying by classname because I think it's generated?
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if(message == "toggle"){
-        toggle();
-    }
-})
+//   });
 
-var iframe = document.createElement('iframe'); 
-iframe.style.background = backgroundColor;
-iframe.style.height = "100%";
-iframe.style.position = "fixed";
-iframe.style.top = "0px";
-iframe.style.right = "0px";
-iframe.style.zIndex = "9000000000000000000";
-iframe.style.border = "0px"; 
-iframe.src = chrome.runtime.getURL("popup.html");
+function setupOnMessageListener() {
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message == "toggle") {
+            toggleTwitterTweaked();
+        }
+    });
+}
 
-document.body.appendChild(iframe);
+function setupSidePanelCSS() {
+    var iframe = document.createElement('iframe');
+    iframe.style.background = backgroundColor;
+    iframe.style.height = "100%";
+    iframe.style.position = "fixed";
+    iframe.style.top = "0px";
+    iframe.style.right = "0px";
+    iframe.style.zIndex = "9000000000000000000";
+    iframe.style.border = "1px";
+    iframe.src = chrome.runtime.getURL("popup.html");
+    document.body.appendChild(iframe);
+    return iframe;
+}
 
-function toggle(){
+function handleEventsPostLoading() {
+    window.addEventListener("load", function load(event) {
+        setupTwitterTweaked();
+        setBorderColor();
+        
+        this.window.removeEventListener("load", load, false);
+    });
+}
 
+function setBorderColor() {
+    try {
+        elemsWithBorderColor = document.getElementsByClassName("r-14wv3jr");
+        var arr = Array.from(elemsWithBorderColor);
+        borderColor = window.getComputedStyle(arr[0]).borderColor;
+    } catch (e) { }
+}
+
+function setupTwitterTweaked() {
+    chrome.storage.local.get({ isTwitterTweakedVisible: true }, function (result) {
+        toggleSidePanelVisibility(result.isTwitterTweakedVisible);
+        toggleTwitterTweakedVisibility(result.isTwitterTweakedVisible);
+        chrome.storage.local.set({ isTwitterTweakedVisible: result.isTwitterTweakedVisible });
+    });
+}
+
+function toggleTwitterTweaked(){
 
     chrome.storage.local.get("isTwitterTweakedVisible", function(result){
         newIsTwitterTweakedVisible = !result.isTwitterTweakedVisible;
-        toggleTwitterSidePanel(newIsTwitterTweakedVisible);
-        toggleTwitterTweaked(newIsTwitterTweakedVisible);
-        // Maybe the line below this is not needed
+        toggleSidePanelVisibility(newIsTwitterTweakedVisible);
+        toggleTwitterTweakedVisibility(newIsTwitterTweakedVisible);
         chrome.storage.local.set({isTwitterTweakedVisible: newIsTwitterTweakedVisible});
     })
 
-
 }
 
-function toggleTwitterSidePanel(isTwitterTweakedVisible) {
-    console.log("Called Function")
+function toggleSidePanelVisibility(isTwitterTweakedVisible) {
     Array.from(document.querySelectorAll("[data-testid=sidebarColumn]"))
     .forEach(function(val) {
         if (val != null) {
@@ -57,7 +84,7 @@ function toggleTwitterSidePanel(isTwitterTweakedVisible) {
     });
 }
 
-function toggleTwitterTweaked(isTwitterTweakedVisible){
+function toggleTwitterTweakedVisibility(isTwitterTweakedVisible){
     if(isTwitterTweakedVisible){
         iframe.style.width="400px";
     }
