dde6ce507855aea68c900f404d4e3d72c4f609de
Kaartik Nagarajan <kaartik@counselab.com>
2022-11-14 17:26:13 +0000

    Changed test to prod

--- src/services/const.js
+++ src/services/const.js
@@ -1,4 +1,4 @@
 
 
-export const URL_NAME = "http://127.0.0.1:8080/"
-// export const URL_NAME = "https://stingray-app-4mn5h.ondigitalocean.app/"
+// export const URL_NAME = "http://127.0.0.1:8080/"
+export const URL_NAME = "https://stingray-app-4mn5h.ondigitalocean.app/"
